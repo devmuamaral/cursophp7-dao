cd7e1603b1f1c4cfec7f964cdb5f077285644b24
Murilo Amaral <devmuamaral@gmail.com>
2020-11-18 15:41:49 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,10 +2,27 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega 1 usuario
 
-$root->loadById(16);
+//$root = new Usuario();
+//$root->loadById(16);
+//echo $root;
 
-echo $root;
+//carrega lista de usuarios
+//método getList é estático portanto nao precisa o objeto ser instanciado
+//$lista = Usuario::getList();
+//echo json_encode($lista);
+
+//Carrega lista buscando por login
+//$search = Usuario::search("o");
+//echo json_encode($search);
+
+//Carrega um usuario usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("pombo", "000");
+
+echo $usuario;
+
 
 ?>
